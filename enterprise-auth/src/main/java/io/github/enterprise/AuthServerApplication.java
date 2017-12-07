1080bcd620b32016da783eecef97e33584c33537
Sheldon Chen <sheldon@shinetour.com>
2017-12-07 14:16:22 +0800

    去掉 spring security 和 oauth2 相关依赖。
    添加 spring data jpa 相关设置。准备实现简单的权限验证框架

--- enterprise-auth/src/main/java/io/github/enterprise/AuthServerApplication.java
+++ enterprise-auth/src/main/java/io/github/enterprise/AuthServerApplication.java
@@ -2,13 +2,13 @@ package io.github.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Sheldon on 2017/12/06
  */
 @SpringBootApplication
-@EnableAuthorizationServer
+@EnableDiscoveryClient
 public class AuthServerApplication {
 
     public static void main(String[] args) {
