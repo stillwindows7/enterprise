b0e535799bc62bdb6824cb0d9f86cb32474b2d7e
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2017-12-13 23:51:24 +0800

    新增针对是否存在 access token 的方法的单元测试
    新增针对查询 access token 返回对应的user 的单元测试
    
    新增 redis 的支持
    
    重构测试

--- enterprise-auth/src/main/java/io/github/enterprise/config/RedisConfig.java
+++ enterprise-auth/src/main/java/io/github/enterprise/config/RedisConfig.java
@@ -2,11 +2,8 @@ package io.github.enterprise.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by sheldon on 2017/12/13.
@@ -15,26 +12,15 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // todo 设置分配到配置文件里，先这么写着吧
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(5);
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
-
-        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
-        connectionFactory.setUsePool(true);
-        connectionFactory.setHostName("localhost");
-        connectionFactory.setPort(6379);
-
-        return connectionFactory;
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
     }
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate() {
-        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setEnableTransactionSupport(true);
-        return stringRedisTemplate;
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(this.jedisConnectionFactory());
+        return template;
     }
 
 }
--- enterprise-auth/src/test/java/io/github/enterprise/service/impl/DefaultAccessTokenServiceTests.java
+++ enterprise-auth/src/test/java/io/github/enterprise/service/impl/DefaultAccessTokenServiceTests.java
@@ -1,7 +1,13 @@
 package io.github.enterprise.service.impl;
 
-import java.util.Optional;
-
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import io.github.enterprise.AuthServerApplication;
+import io.github.enterprise.model.LocalAuth;
+import io.github.enterprise.model.User;
+import io.github.enterprise.repository.LocalAuthRepository;
+import io.github.enterprise.repository.UserRepository;
+import io.github.enterprise.service.AccessTokenService;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,62 +16,104 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-
-import io.github.enterprise.model.LocalAuth;
-import io.github.enterprise.model.User;
-import io.github.enterprise.repository.LocalAuthRepository;
-import io.github.enterprise.repository.UserRepository;
-import io.github.enterprise.service.AccessTokenService;
+import java.util.Optional;
 
 /**
  * Created by Sheldon on 2017年12月13日
- *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @TestExecutionListeners({
-		DependencyInjectionTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class,
         DirtiesContextTestExecutionListener.class,
         TransactionalTestExecutionListener.class,
         DbUnitTestExecutionListener.class})
 @DatabaseSetup("/META-INF/dbtest/default_access_token_sample_data.xml")
 public class DefaultAccessTokenServiceTests {
 
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private LocalAuthRepository localAuthRepository;
-	
-	@Autowired
-	private AccessTokenService accessTokenService;
-	
-	@Before
-	public void setUp() {
-		
-	}
-	
-	@After
-	public void tearDown() {
-		this.userRepository.deleteAll();
-	}
-	
-	@Test
-	public void testGenerateAccessToken() {
-		Optional<LocalAuth> sheldonAuthOptional = this.localAuthRepository
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private LocalAuthRepository localAuthRepository;
+
+    @Autowired
+    private AccessTokenService accessTokenService;
+
+    private String accessToken;
+
+    @Before
+    public void setUp() {
+        Optional<LocalAuth> sheldonAuthOptional = this.localAuthRepository
                 .findByUsernameAndPassword("sheldonchen", "123456Ab");
-		Assert.assertTrue("user must be exist", sheldonAuthOptional.isPresent());
-		
-		LocalAuth sheldonAuth = sheldonAuthOptional.get();
-		User user = sheldonAuth.getUser();
-		String accessToken = this.accessTokenService.generateAccessToken(user);
-		Assert.assertNotNull("access token cannot be empty", accessToken);
-	}
+        Assert.assertTrue("user must be exist", sheldonAuthOptional.isPresent());
+
+        LocalAuth sheldonAuth = sheldonAuthOptional.get();
+        User user = sheldonAuth.getUser();
+        this.accessToken = this.accessTokenService.generateAccessToken(user);
+    }
+
+    @After
+    public void tearDown() {
+        this.userRepository.deleteAll();
+    }
+
+    /**
+     * 测试生成 access token 方法
+     */
+    @Test
+    public void testGenerateAccessToken() {
+        Assert.assertNotNull("access token cannot be empty", this.accessToken);
+    }
+
+    /**
+     * 测试 access token 是否存在，此方法测试存在的 access token
+     * <p>
+     * 应该返回 true
+     */
+    @Test
+    public void testCheckSuccess() {
+        boolean result = this.accessTokenService.check(accessToken);
+        Assert.assertTrue("this access token exist", result);
+    }
+
+    /**
+     * 测试 access token 是否存在，此方法测试不存在的 access token
+     * <p>
+     * 应该返回 false
+     */
+    @Test
+    public void testCheckFail() {
+        boolean result = this.accessTokenService.check("wrong access token");
+        Assert.assertFalse("this access token is not exist", result);
+    }
+
+    /**
+     * 测试 根据 access token 返回 User 对象方法，此方法测试正确的 access token
+     * <p>
+     * 应该返回正确的 Optional 对象
+     */
+    public void testFindbyAccessTokenSuccess() {
+        Optional<User> userOptional = this.accessTokenService.findByAccessToken(this.accessToken);
+        Assert.assertTrue("user must exist", userOptional.isPresent());
+
+        User user = userOptional.get();
+        Assert.assertEquals("user id must equals 1", "1", user.getId());
+    }
+
+    /**
+     * 测试 根据 access token 返回 User 对象方法，此方法测试错误的 access token
+     * <p>
+     * 应该返回空的 Optional 对象
+     */
+    public void testFindByAccessTokenFail() {
+        Optional<User> userOptional = this.accessTokenService.findByAccessToken("wrong access token");
+        Assert.assertFalse("user must not exist", userOptional.isPresent());
+    }
+
 }
